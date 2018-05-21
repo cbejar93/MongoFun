b04aabcc3c0831654db92cc76047909ca44ba833
unknown <cbejar93@gmail.com>
2018-05-21 19:43:34 -0400

    h app

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.use(bodyParser.urlencoded({
 var db = require("./models");
 app.use(logger("dev"));
 
-mongoose.connect("mongodb://localhost/news_db");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news_db";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 var routes = require("./routes/mongoAPI");
 app.use(routes);
